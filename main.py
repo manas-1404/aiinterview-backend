f9c04f6cb5aa0f78de981e292f4c4787c31d83df
manas-1404 <manas.gowda04@gmail.com>
2025-08-02 18:55:46 +0200

    added all routers to app

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from ai_interviewer_sdk import FoundryClient, UserTokenAuth
 
 from pydantic_schemas.response_pydantic import ResponseSchema
 from utils.config import settings
-
+from routes.logic_route import login_router
+from routes.turn_route import turn_route
+from routes.dashboard_route import dashboard_router
+from routes.uploadfile_route import upload_router
+from routes.interviewagent_route import agent_router
+from routes.practice_route import practice_router
 app = FastAPI()
 
 app.add_middleware(
@@ -19,6 +24,14 @@ app.add_middleware(
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+app.include_router(login_router)
+app.include_router(turn_route)
+app.include_router(dashboard_router)
+app.include_router(upload_router)
+app.include_router(agent_router)
+app.include_router(practice_router)
+
 
 @app.on_event("startup")
 async def startup_event():
